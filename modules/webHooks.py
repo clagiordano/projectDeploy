84fd0c2d428997c1648de649ec4f14b97eaa0cb5
Claudio Giordano <claudio.giordano@autistici.org>
2016-09-05 13:13:48 +0200

    Added new webhooks module

---
+++ modules/webHooks.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Generic webHooks support with urllib2"""
+
+import urllib
+
+class webHooks(object):
+    baseurl = None
+    requesturl = "None"
+    payload = None
+    headers = None
+    method = 'GET'
+
+    def setBaseUrl(self, string):
+        self.baseurl = string
+
+    def setRequestUrl(self, string):
+        self.requesturl = string
+
+    def setPayload(self, payload):
+        self.payload = payload
+
+    def setHeaders(self, headers):
+        self.headers = headers
+
+    def setMethod(self, string):
+        self.method = string
+
+    def doRequest(self):
+        if self.baseurl == None:
+            return False
+
+        if self.requesturl == None:
+            return False
+
+        request = urllib.urlopen(self.baseurl + self.requesturl, self.payload)
+        return request.read()
+
+# if __name__ == "__main__":
+#     wh = webHooks()
+#     wh.setBaseUrl('https://hooks.slack.com/')
+#     wh.setRequestUrl('services/T03N7DSBC/B27MNPKSQ/lNrOStWYjhE55iQbguudOfII')
+#     wh.setPayload('{ "attachments": [{ "color": "#DDB100", "title": "TEST", "fallback": "TEST", "fields": [{"title": "Project", "value": "TEST", "short": false }, {"title": "Source hostname", "value": "TEST", "short": false}]}]}')
+#
+#     print wh.doRequest()
